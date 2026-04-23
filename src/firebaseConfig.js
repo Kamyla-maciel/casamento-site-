0cece86f7bd9d436ffd80f193a46e4dbf273bab0
Kamyla-maciel <13525121862@ulife.com.br>
2026-04-23 01:40:48 -0300

    Agora sim o firebaseConfig esta na src

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4GTZifg6T60JtiuaNf-PkWI6UI19EJ5g",
+  authDomain: "casamento-rsvp-e2d36.firebaseapp.com",
+  projectId: "casamento-rsvp-e2d36",
+  storageBucket: "casamento-rsvp-e2d36.firebasestorage.app",
+  messagingSenderId: "929465334271",
+  appId: "1:929465334271:web:f898fac2a75de1180a8fca",
+  measurementId: "G-TGB7DNDPNT"
+};
+
+const app = initializeApp(firebaseConfig);
